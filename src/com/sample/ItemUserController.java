9a7e67b5a5151db6142ecc8c595fc16e9eae8778
tarovinl <karl.tacula.cics@ust.edu.ph>
2024-11-10 23:50:53 +0800

    item user update

--- src/com/sample/ItemUserController.java
+++ src/com/sample/ItemUserController.java
@@ -12,8 +12,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet("/itemUser")
 public class ItemUserController extends HttpServlet {
@@ -22,7 +24,11 @@ public class ItemUserController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         List<ItemUser> itemUserList = new ArrayList<>();
-        String query = "SELECT EMP_NUMBER, USER_TYPE FROM C##FMO_ADM.FMO_ITEM_USERS";
+
+        // Query for ItemUser data
+        String query = "SELECT iu.EMP_NUMBER, iu.USER_TYPE, e.FIRST_NAME, e.SURNAME " +
+                       "FROM C##FMO_ADM.FMO_ITEM_USERS iu " +
+                       "LEFT JOIN C##FMO_ADM.FMO_EMPLOYEES e ON iu.EMP_NUMBER = e.EMP_NUMBER";
 
         try (Connection connection = PooledConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(query);
@@ -32,16 +38,62 @@ public class ItemUserController extends HttpServlet {
                 ItemUser itemUser = new ItemUser();
                 itemUser.setEmpNumber(resultSet.getInt("EMP_NUMBER"));
                 itemUser.setUserType(resultSet.getInt("USER_TYPE"));
-                
+
+                String firstName = resultSet.getString("FIRST_NAME");
+                String surname = resultSet.getString("SURNAME");
+                String fullName = (firstName != null && surname != null) 
+                                  ? firstName + " " + surname 
+                                  : "N/A";
+                itemUser.setFullName(fullName);
+
                 itemUserList.add(itemUser);
             }
         } catch (Exception e) {
             throw new ServletException("Error retrieving item user data", e);
         }
 
-        request.setAttribute("itemUserList", itemUserList);
-        request.getRequestDispatcher("itemUser.jsp").forward(request, response);
-        System.out.println("Retrieved item users: " + itemUserList.size());
+        // Define the userTypeList as a list of key-value pairs for dropdown options
+        List<Map.Entry<Integer, String>> userTypeList = new ArrayList<>();
+        userTypeList.add(new AbstractMap.SimpleEntry<>(1, "Admin"));
+        userTypeList.add(new AbstractMap.SimpleEntry<>(2, "View Only"));
 
+        // Set attributes for the JSP
+        request.setAttribute("itemUserList", itemUserList);
+        request.setAttribute("userTypeList", userTypeList);
+
+        // Forward to itemUser.jsp
+        request.getRequestDispatcher("itemUser.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get parameters from the form
+        int empNumber = Integer.parseInt(request.getParameter("empNumber"));
+        int userType = Integer.parseInt(request.getParameter("userType"));
+
+        // SQL MERGE statement for insert or update
+        String insertOrUpdateQuery = 
+            "MERGE INTO C##FMO_ADM.FMO_ITEM_USERS iu " +
+            "USING (SELECT ? AS EMP_NUMBER, ? AS USER_TYPE FROM dual) new_values " +
+            "ON (iu.EMP_NUMBER = new_values.EMP_NUMBER) " +
+            "WHEN MATCHED THEN UPDATE SET iu.USER_TYPE = new_values.USER_TYPE " +
+            "WHEN NOT MATCHED THEN INSERT (EMP_NUMBER, USER_TYPE) VALUES (new_values.EMP_NUMBER, new_values.USER_TYPE)";
+
+        try (Connection connection = PooledConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(insertOrUpdateQuery)) {
+
+            // Set parameters
+            statement.setInt(1, empNumber);
+            statement.setInt(2, userType);
+
+            // Execute update or insert
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new ServletException("Error saving item user data", e);
+        }
+
+        // Redirect to doGet to refresh the page and display the updated list
+        response.sendRedirect("itemUser");
     }
 }
--- src/sample/model/ItemUser.java
+++ src/sample/model/ItemUser.java
@@ -3,6 +3,10 @@ package sample.model;
 public class ItemUser {
     private int empNumber;
     private int userType;
+    private String fullName;
+    private String userTypeString;  
+
+    // Getters and Setters
 
     public int getEmpNumber() {
         return empNumber;
@@ -18,5 +22,22 @@ public class ItemUser {
 
     public void setUserType(int userType) {
         this.userType = userType;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    // New getter and setter for userTypeString
+    public String getUserTypeString() {
+        return userTypeString;
+    }
+
+    public void setUserTypeString(String userTypeString) {
+        this.userTypeString = userTypeString;
     }
 }
